f3b3299f8b2ffc86549127f99ec92e9b4b871f08
Iftikhar Khan <iakhan4425@gmail.com>
2018-10-18 00:43:43 -0400

    Used the modified typedef to make a function call

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 /******************************************************************************
   Title          : main.cpp
-  Author         : 
+  Author         : Iftikhar Khan (f0cus10)
   Created on     : October  7, 2018
   Description    : Displays random peculiar facts
   Purpose        : To exercise collaborative source code development
   Usage          : nuttyfact
   Build with     : g++ -Wall -g -o oddities  main.cpp
-  Modifications  :
+  Modifications  : Added intro, changed typedef
   Notes:
   The number N below is the number of people on each team.
   This program prints N peculiar facts, called "oddities" here.
@@ -35,7 +35,7 @@ using namespace std;
 /******************************************************************************/
 
 // The syntax of the output function that must be used to print an oddity.
-typedef ostream& (*output_function) (const ostream & out);
+typedef ostream& (*output_function) (ostream & out);
 
 
 /******************************************************************************
@@ -45,16 +45,18 @@ typedef ostream& (*output_function) (const ostream & out);
 ******************************************************************************/
 
 
+ostream& printer(ostream& out){
+    out << "This is a collection of strange but true facts:" << endl;
+    return out;
+}
 
-
-
+output_function intro = &printer;
 
 int main(int argc, char* argv[] )
 {
-
     // Calls to output functions
-
-
+    (*intro)(cout);
 
     return 0;
 }
+
